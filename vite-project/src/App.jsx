2da0017c8c975ee06227031c21d97ce309e0556a
parkyugyeong <qkrb8019@gmai.com>
2024-12-02 13:08:47 +0900

    movielist 페이지 퍼블리싱

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/globalStyles";
+import Upload from "./pages/upload";
+import MovieList from "./pages/MovieList";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <div>
-        <h1>Hello, Movie Review!</h1>
-      </div>
-    </>
+      <Routes>
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/list" element={<MovieList />} />
+      </Routes>
+    </Router>
   );
 }
 
