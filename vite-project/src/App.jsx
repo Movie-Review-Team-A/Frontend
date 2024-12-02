4f93211cc88f29ab7a4a8e24ad6d2b89448822eb
parkyugyeong <qkrb8019@gmai.com>
2024-12-02 13:11:05 +0900

    app.jsx 수정

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/globalStyles";
-import Upload from "./pages/upload";
 import MovieList from "./pages/MovieList";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <GlobalStyle />
       <Routes>
-        <Route path="/upload" element={<Upload />} />
         <Route path="/list" element={<MovieList />} />
       </Routes>
     </Router>
